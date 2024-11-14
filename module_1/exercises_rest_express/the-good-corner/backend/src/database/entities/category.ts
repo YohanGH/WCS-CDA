85c987e98f10c9b3c3260814bd2032f399e7c496
YohanGH <regnier.yohan@proton.me>
2024-11-14 12:30:28 +0100

    Update the entities to use GraphQL

--- module_1/exercises_rest_express/the-good-corner/backend/src/database/entities/category.ts
+++ module_1/exercises_rest_express/the-good-corner/backend/src/database/entities/category.ts
@@ -1,15 +1,22 @@
 import { BaseEntity, Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { ObjectType, Field, ID } from 'type-graphql';
 import { Ad } from "./ad";
 
-
-@Entity({ name: 'category'})
+// This class represents a category entity in the database.
+// It defines the structure of a category, including its unique identifier (id),
+// the title of the category, and a one-to-many relationship with ads.
+@ObjectType()
+@Entity({ name: 'category' })
 export class Category extends BaseEntity {
+    @Field(() => ID)
     @PrimaryGeneratedColumn()
-    id!: number;
+    id!: number; // Unique identifier for the category
 
+    @Field()
     @Column({ length: 255 })
-    title!: string;
+    title!: string; // Title of the category
 
-    @OneToMany(() => Ad, (ad) => ad.category)
-    ads!: Ad[];
+    @Field(() => [Ad], { nullable: true }) // List of ads associated with this category
+    @OneToMany(() => Ad, (ad) => ad.category) // One-to-many relationship with Ads
+    ads?: Ad[]; // List of ads associated with this category
 }
