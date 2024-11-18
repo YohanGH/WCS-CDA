a990798640cf265b82134fcd09cc5345caa68ffd
YohanGH <regnier.yohan@proton.me>
2024-11-18 14:39:52 +0100

    Update style in App.tsx and create App.module.css

--- module_1/exercises_vite_react/the-good-corner/frontend/src/App.tsx
+++ module_1/exercises_vite_react/the-good-corner/frontend/src/App.tsx
@@ -1,13 +1,36 @@
 import { Outlet } from "react-router-dom";
 import Footer from "./components/sections/Footer";
 import Header from "./components/sections/Header";
+import styles from "./styles/App.module.css";
 
 function App() {
   return (
     <>
       <Header />
-      <main className="main-content" aria-label="Main Content">
-        <Outlet />
+      <main
+        className={`${styles.mainContent}`}
+        aria-label="Main Content"
+      >
+        {/* Responsive Background */}
+        <div
+          className={`${styles.mobileBackground} sm:hidden`}
+          style={{
+            backgroundImage: `url('/images/mobileSection.png')`,
+          }}
+        />
+        <div
+          className={`${styles.desktopBackground} hidden sm:block`}
+          style={{
+            backgroundImage: `url('/images/defaultSection.png')`,
+          }}
+        />
+
+        {/* Dot Pattern Overlay */}
+        <div className={styles.dotPatternOverlay} />
+
+        <div className={styles.contentWrapper}>
+          <Outlet />
+        </div>
       </main>
       <Footer />
     </>
